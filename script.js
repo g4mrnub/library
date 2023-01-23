557f198be2afbd20d669108b6cfa67c72c842cc7
g4mrnub <g4mrnub@gmail.com>
2023-01-22 19:38:50 -0600

    add book class

--- script.js
+++ script.js
@@ -1,107 +1,87 @@
-let myLibrary = [
-  ];
+let myLibrary = [];
 
-const libraryContainer = document.querySelector('#library');
-const overlayContainer = document.querySelector('body');
+const libraryContainer = document.querySelector("#library");
+const overlayContainer = document.querySelector("body");
 
-function BookOverlord(){
-}
-
-BookOverlord.prototype.readStatusToggle = function(){
-  if (!this.read) {
-    this.read = true;
-  }else{
-    this.read = false;
+class Book {
+  constructor(title, author, pages) {
+    // the constructor...
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    let read = false;
   }
+
+  readStatusToggle = function () {
+    if (!this.read) {
+      this.read = true;
+    } else {
+      this.read = false;
+    }
+  };
 }
 
-function Book(title, author, pages) {
-  // the constructor...
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  let read = false;
-}
+function addBookToLibrary(formData) {
+  //let  formData = document.getElementById("bookForm"); //grab form from DOM
 
-Book.prototype = Object.create(BookOverlord.prototype);
+  let bookName = formData.title.value;
+  let bookAuthorLabel = formData.author.value;
+  let bookPagesLabel = formData.pages.value;
 
-function addBookToLibrary(formData){
-  
-    //let  formData = document.getElementById("bookForm"); //grab form from DOM
-
-    let bookName = formData.title.value;
-    let bookAuthorLabel = formData.author.value;
-    let bookPagesLabel = formData.pages.value;
-
-    let book1 = new Book(bookName, bookAuthorLabel, bookPagesLabel); //make new book object from form DOM data
+  let book1 = new Book(bookName, bookAuthorLabel, bookPagesLabel); //make new book object from form DOM data
   myLibrary.push(book1); //push that onto an array stack of book objects
 }
 
-let removeBookfromLibrary = function(library, book){ //loops through whole library array
+let removeBookfromLibrary = function (library, book) {
+  //loops through whole library array
   let removalIndex = null;
-  for(let i = 0; i<library.length;i++){
-    if(library[i].title == book.title){ //finds matching title and index
+  for (let i = 0; i < library.length; i++) {
+    if (library[i].title == book.title) {
+      //finds matching title and index
       removalIndex = i;
     }
   }
-  library.splice(removalIndex,1); //removes that index
+  library.splice(removalIndex, 1); //removes that index
   displayLibrary(myLibrary);
-}
-
-/*let findArrayBook = function(library, domBook){ //loops through whole library array
-  let bookIndex = null;
-  for(let i = 0; i<library.length;i++){
-    if(library[i].title == domBook.title){ //domBook NEEDS A DATA KEY INSTEAD BECAUSE p ELEMENTS IN NORMAL DIVS ARE ANONYMOUS WITHOUT IDs
-                                            //so it doesn't even know what "title" property is since it doesn't have a "name" value.
-                                            //I PASSED AN ARRAY OBJECT NOT DOM OBJECT INTO book argument last time because
-                                            //its array index was inside a for loop iteration and not globally scoped.
-                                            //I need to still pass an arrayBook instead then but find it with the data key attribute
-                                            //and use that in the book array index
-      bookIndex = i;
-    }
-  }
-  return library[bookIndex];
-}
-*/
+};
 
 function addBookForm() {
-  
   const filter = document.createElement("div");
-  filter.classList.add('filter');
+  filter.classList.add("filter");
 
   const bookForm = document.createElement("form");
-  bookForm.setAttribute("id","bookForm"); 
-  bookForm.classList.add('bookForm');
+  bookForm.setAttribute("id", "bookForm");
+  bookForm.classList.add("bookForm");
 
   const bookTitleLabel = document.createElement("label");
-  bookTitleLabel.setAttribute("for","title"); 
+  bookTitleLabel.setAttribute("for", "title");
   bookTitleLabel.textContent = "Book Title:";
   const bookTitleBox = document.createElement("input");
-  bookTitleBox.setAttribute('type',"text");
-  bookTitleBox.setAttribute('name',"title"); 
+  bookTitleBox.setAttribute("type", "text");
+  bookTitleBox.setAttribute("name", "title");
   bookTitleBox.setAttribute("required", "");
 
   const bookAuthorLabel = document.createElement("label");
-  bookAuthorLabel.setAttribute('for',"author"); 
+  bookAuthorLabel.setAttribute("for", "author");
   bookAuthorLabel.textContent = "Book Author:";
   const bookAuthorBox = document.createElement("input");
-  bookAuthorBox.setAttribute('type',"text"); 
-  bookAuthorBox.setAttribute('name',"author"); 
-  bookAuthorBox.setAttribute("required", "")
+  bookAuthorBox.setAttribute("type", "text");
+  bookAuthorBox.setAttribute("name", "author");
+  bookAuthorBox.setAttribute("required", "");
 
   const bookPagesLabel = document.createElement("label");
-  bookPagesLabel.setAttribute('for',"pages"); 
+  bookPagesLabel.setAttribute("for", "pages");
   bookPagesLabel.textContent = "Page Count:";
   const bookPagesBox = document.createElement("input");
-  bookPagesBox.setAttribute('type',"number"); 
-  bookPagesBox.setAttribute('name',"pages"); 
+  bookPagesBox.setAttribute("type", "number");
+  bookPagesBox.setAttribute("name", "pages");
   bookPagesBox.setAttribute("required", "");
 
   const submit = document.createElement("button");
-  submit.setAttribute('type',"submit");
-  submit.classList.add('addBook'); 
+  submit.setAttribute("type", "submit");
+  submit.classList.add("addBook");
   submit.textContent = "SUBMIT";
-  bookForm.addEventListener('submit', (event) => {
+  bookForm.addEventListener("submit", (event) => {
     addBookToLibrary(event.currentTarget);
     displayLibrary(myLibrary);
     bookForm.parentNode.removeChild(bookForm);
@@ -111,32 +91,33 @@ function addBookForm() {
   overlayContainer.appendChild(filter);
   overlayContainer.appendChild(bookForm);
   bookForm.append(
-    bookTitleLabel, 
-    bookTitleBox, 
-    bookAuthorLabel, 
-    bookAuthorBox, 
-    bookPagesLabel, 
-    bookPagesBox, 
-    submit);
+    bookTitleLabel,
+    bookTitleBox,
+    bookAuthorLabel,
+    bookAuthorBox,
+    bookPagesLabel,
+    bookPagesBox,
+    submit
+  );
 }
 
-function displayLibrary(library){
+function displayLibrary(library) {
   libraryContainer.innerHTML = ""; //clear and refresh content
   let bookIndex = 0;
-  library.forEach(book => {
+  library.forEach((book) => {
     const bookDisplay = document.createElement("div");
-    bookDisplay.classList.add('bookDisplay');
+    bookDisplay.classList.add("bookDisplay");
     const bookContentGrid = document.createElement("div");
-    bookContentGrid.classList.add('bookContentGrid');
+    bookContentGrid.classList.add("bookContentGrid");
 
-    bookDisplay.setAttribute('data-id', `${bookIndex}`);
+    bookDisplay.setAttribute("data-id", `${bookIndex}`);
     bookIndex += 1;
 
     const bookTitleLabel = document.createElement("p");
-    bookTitleLabel.classList.add('title');
+    bookTitleLabel.classList.add("title");
     bookTitleLabel.textContent = `Title:`;
     const bookTitle = document.createElement("p");
-    bookTitle.classList.add('title');
+    bookTitle.classList.add("title");
     bookTitle.textContent = `${book.title}`;
 
     const bookAuthorLabel = document.createElement("p");
@@ -152,40 +133,24 @@ function displayLibrary(library){
     const bookRead = document.createElement("p");
     bookRead.textContent = "Read:";
     const readBox = document.createElement("input");
-    readBox.setAttribute('type',"checkbox"); 
-    if (book.read == true){
-      readBox.setAttribute('checked', ""); 
+    readBox.setAttribute("type", "checkbox");
+    if (book.read == true) {
+      readBox.setAttribute("checked", "");
     }
-    bookDisplay.addEventListener('click', (event) => {
-      if(event.target == readBox){
+    bookDisplay.addEventListener("click", (event) => {
+      if (event.target == readBox) {
         book.readStatusToggle();
       }
-
-      //OKAY GOT IT.
-      /* ((rn I find array books by matching DOM and object VALUES) this time I assign new 
-        books KEYS and match datakeys to array indices. I display books one at time,)=OPTIONAL ACTUALLY.
-        I delegate the "find array book" to a separate function which returns the book object inside the array, so the "book" can be found
-        in a global context instead of the for loop where scope is lost in callback function.
-        findArrayBook(myLibrary, event.currentTarget).readStatusToggle();
-
-        okay you don't need the findArray function if you just use the datakey from the dom element inside the library array index
-        myLibrary[event.currentTarget.getAttribute('data-id')].readStatusToggle();
-
-        WOW LOL the "readStatusToggle() is not a function" error was from my hardcoded book cards that weren't constructed
-        with the constructor/protype function assignment. The scope of the event listener callback was completely fine...
-        It at least got me to make a real protype chain with correct memory use of the function but the data-id was not needed or 
-        my attempt at the findArrayBook function.
-      */
     });
 
     const removeButton = document.createElement("button");
-    removeButton.classList.add('x');
+    removeButton.classList.add("x");
     removeButton.textContent = "X";
-    removeButton.addEventListener('click', () => {
+    removeButton.addEventListener("click", () => {
       removeBookfromLibrary(myLibrary, book);
     });
 
-    libraryContainer.appendChild(bookDisplay)
+    libraryContainer.appendChild(bookDisplay);
     bookDisplay.appendChild(bookContentGrid);
     bookContentGrid.append(
       bookTitleLabel,
@@ -195,9 +160,8 @@ function displayLibrary(library){
       bookPagesLabel,
       bookPages,
       bookRead,
-      readBox,
+      readBox
     );
     bookDisplay.appendChild(removeButton);
   });
-
 }
